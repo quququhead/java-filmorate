3ff14a10acf08c50e13614b1485127ad7fbd2d41
Lucky7456 <amir.abdrahmanov@yandex.ru>
2024-11-08 16:24:09 +0500

    feat: add feed

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,17 +2,14 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dal.FilmGenreRepository;
-import ru.yandex.practicum.filmorate.dal.GenreRepository;
-import ru.yandex.practicum.filmorate.dal.LikeRepository;
-import ru.yandex.practicum.filmorate.dal.MpaRepository;
+import ru.yandex.practicum.filmorate.dal.*;
 import ru.yandex.practicum.filmorate.dal.interfaces.FilmStorage;
 import ru.yandex.practicum.filmorate.dal.interfaces.UserStorage;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.FilmGenre;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.model.*;
+import ru.yandex.practicum.filmorate.model.enums.EventType;
+import ru.yandex.practicum.filmorate.model.enums.Operation;
 
+import java.time.Instant;
 import java.util.*;
 
 @Service
@@ -24,6 +21,7 @@ public class FilmService {
     private final GenreRepository genreRepository;
     private final LikeRepository likeRepository;
     private final MpaRepository mpaRepository;
+    private final FeedRepository feedRepository;
 
     public Collection<Film> findAllFilms() {
         return prepare(filmStorage.getAllFilms());
@@ -34,9 +32,7 @@ public class FilmService {
     }
 
     public Film findFilm(long filmId) {
-        Film film = filmStorage.getFilm(filmId);
-        checkFilmNotNull(film);
-        return prepare(film);
+        return prepare(notNull(filmStorage.getFilm(filmId)));
     }
 
     public Film createFilm(Film film) {
@@ -45,25 +41,34 @@ public class FilmService {
     }
 
     public Film updateFilm(Film newFilm) {
-        checkFilmNotNull(filmStorage.getFilm(newFilm.getId()));
+        notNull(filmStorage.getFilm(newFilm.getId()));
         filmGenreRepository.deleteGenresFromFilm(newFilm.getId());
         return process(filmStorage.updateFilm(newFilm));
     }
 
     public void setLike(long id, long userId) {
-        userStorage.getUser(userId);
+        notNull(userStorage.getUser(userId));
         likeRepository.insert(id, userId);
+        feedRepository.create(new Feed(userId, EventType.LIKE, Operation.ADD, id, Instant.now().toEpochMilli()));
     }
 
     public void deleteLike(long id, long userId) {
-        userStorage.getUser(userId);
+        notNull(userStorage.getUser(userId));
         likeRepository.delete(id, userId);
+        feedRepository.create(new Feed(userId, EventType.LIKE, Operation.REMOVE, id, Instant.now().toEpochMilli()));
     }
 
-    private void checkFilmNotNull(Film film) {
+    private void notNull(User user) {
+        if (user == null) {
+            throw new NoSuchElementException("Юзер не найден");
+        }
+    }
+
+    private Film notNull(Film film) {
         if (film == null) {
             throw new NoSuchElementException("Фильм не найден");
         }
+        return film;
     }
 
     private Film process(Film film) {
