6dc6aad8eb8fa38caf369e9df7a5896dd6859605
Lucky7456 <amir.abdrahmanov@yandex.ru>
2024-11-08 10:20:02 +0500

    fix: tests; codestyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -2,22 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Review {
-    private Long reviewId;
-    @NotBlank
-    private String content;
-    @NotNull
-    private Boolean isPositive;
-    @NotNull
-    private Long userId;
-    @NotNull
-    private Long filmId;
-    private Long useful;
+public record Review(
+        Long reviewId,
+        @NotBlank String content,
+        @NotNull Boolean isPositive,
+        @NotNull Long userId,
+        @NotNull Long filmId,
+        Long useful
+) {
 }
--- src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/FilmRepositoryTest.java
@@ -19,17 +19,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @JdbcTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@Import({FilmGenreRepository.class, FilmGenreRowMapper.class,
-        FilmRepository.class, FilmRowMapper.class,
-        FriendRepository.class, FriendRowMapper.class,
-        GenreRepository.class, GenreRowMapper.class,
-        LikeRepository.class, LikeRowMapper.class,
-        MpaRepository.class, MpaRowMapper.class,
-        UserRepository.class, UserRowMapper.class
-})
+@Import({FilmRepository.class, FilmRowMapper.class})
 class FilmRepositoryTest {
     private final FilmRepository filmRepository;
-    private final UserRepository userRepository;
 
     private Film createFilm() {
         Film film = new Film();
@@ -40,16 +32,6 @@ class FilmRepositoryTest {
         film.setId(1);
         film.setMpa(Mpa.builder().id(1).name("R").build());
         return film;
-    }
-
-    private User createUser() {
-        User user = new User();
-        user.setEmail("quququhead@gmail.com");
-        user.setLogin("quhead");
-        user.setBirthday(LocalDate.of(2001, 1, 20));
-        user.setId(1);
-        user.setName("Gleb");
-        return user;
     }
 
     @Test
@@ -75,34 +57,20 @@ class FilmRepositoryTest {
     @Test
     void shouldGetFilm() {
         Film film1 = createFilm();
-        film1 = filmRepository.addFilm(film1);
-        Film film2 = filmRepository.getFilm(film1.getId());
+        Film film2 = filmRepository.getFilm(filmRepository.addFilm(film1));
         assertEquals(film1.getName(), film2.getName());
     }
 
     @Test
     void shouldAddFilm() {
         Film film = createFilm();
-        film = filmRepository.addFilm(film);
-        assertNotEquals(1, film.getId());
-    }
-
-    @Test
-    void shouldSetLike() {
-        User user = createUser();
-        user = userRepository.addUser(user);
-        Film film = createFilm();
-        film = filmRepository.addFilm(film);
-
-        filmRepository.setLike(film.getId(), user.getId());
-        film = filmRepository.getFilm(film.getId());
-        assertEquals(1, film.getLikes().size());
+        assertNotEquals(filmRepository.addFilm(film), film.getId());
     }
 
     @Test
     void shouldUpdateFilm() {
         Film film1 = createFilm();
-        film1 = filmRepository.addFilm(film1);
+        film1.setId(filmRepository.addFilm(film1));
         film1.setName("American Psycho");
         film1.setDescription("Film about Patrick Bateman");
         film1.setReleaseDate(LocalDate.of(2000, 1, 21));
@@ -113,18 +81,5 @@ class FilmRepositoryTest {
         assertEquals(film1.getDescription(), film2.getDescription());
         assertEquals(film1.getReleaseDate(), film2.getReleaseDate());
         assertEquals(film1.getDuration(), film2.getDuration());
-    }
-
-    @Test
-    void shouldDeleteLike() {
-        User user = createUser();
-        user = userRepository.addUser(user);
-        Film film = createFilm();
-        film = filmRepository.addFilm(film);
-
-        filmRepository.setLike(film.getId(), user.getId());
-        filmRepository.deleteLike(film.getId(), user.getId());
-        film = filmRepository.getFilm(film.getId());
-        assertEquals(0, film.getLikes().size());
     }
 }
